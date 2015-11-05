da3c603dd4f66ff74dda9ebed70a8d56e3c38127
it126 <deepali-b@cuelogic>
2015-11-05 20:26:23 +0530

    Fixed naming issues Partial implementation of UI for second step in top collpasible menu

--- static/xantar_app/js/fileuploader.js
+++ static/xantar_app/js/fileuploader.js
@@ -1,6 +1,6 @@
 function progressfun(){
     $('#result').empty();
-    var Files = document.getElementById('fil').files[0]
+    var Files = document.getElementById('choose_file').files[0]
     console.log(Files)
     loaded = 0;
     var fileReader = new FileReader();
@@ -14,12 +14,42 @@ function progressfun(){
             $('#result').empty();
             var progress = parseInt( ((data.loaded / data.total) * 100), 10 );
             console.log(progress);
-            $('#result').append('<div class="progress"><div class="progress-bar" role="progressbar" aria-valuemin="0" aria-valuemax="100" style="width:'+progress+'%"></div>'); 
+            $('#result').append('<div class="well"><div class="progress"><div class="progress-bar" role="progressbar" aria-valuemin="0" aria-valuemax="100" style="width:'+progress+'%"></div></div>');
             if(progress == 100){
-                $('#result').fadeOut(3000);
-                $('#success').append('<div class="alert alert-success"><strong></strong>Successfully Uploaded</div>');
-                $('#success').fadeOut(5000);
+                $('#success').css("display", "block");
+                $('#finish').prop("disabled", false);
+                $('#prev').prop("disabled", true);
             }
         }
 }
 }
+
+function getFileData(myFile){
+       var file = myFile.files[0];  
+           document.getElementById('choose_file_val').innerHTML = file.name;
+}
+
+$("#choose_file").click(function() {
+    $('#success').css("display", "none");
+});
+
+$("#finish").click(function(){
+    if($('#step1').hasClass('active'))
+    {
+        $('#step1tag').removeClass('active');
+        $('#step1').removeClass('active');
+        $('#step1').css("display","none");
+        $('#step2tag').addClass('active');
+        $('#step2').addClass('active');
+        $('#step2').css("display","block");
+    }
+    else{
+        $('#step2tag').removeClass('active');
+        $('#step2').removeClass('active');
+        $('#step2').css("display","none");
+        $('#step3tag').addClass('active');
+        $('#step3').addClass('active');
+        $('#step3').css("display","block");
+    }
+});
+
