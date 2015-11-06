fc4b0d87d70ea8ada1a471cabfb6a0d954f48543
it126 <deepali-b@cuelogic>
2015-11-06 19:50:03 +0530

    Implementing Mapping UI and Final Step UI for top collapsible menu

--- static/xantar_app/js/fileuploader.js
+++ static/xantar_app/js/fileuploader.js
@@ -1,3 +1,17 @@
+var globalfields = ['Country','BRAND_CODE','BRAND_NAME','ADV_CODE','ADV_NAME','DIRECTMAIL FIGURE','DATA MONTH',
+'CURRENCY','DATA FACTOR','NEWSPAPER FIGURE','OUTDOOR_FIGURE','MAGAZINES FIGURE','CINEMA FIGURE','TV FIGURE',
+'INTERNET_FIGURE','RADIO FIGURE','LEVEL1 CODE','LEVEL2 CODE','LEVEL3 CODE','LEVEL3 NAME'];
+var active = 'globallist';
+var flag_upload,flag_map =0;
+function populatedropdown(){
+$.each( globalfields, function( index, value ){
+    $('#globallist').append($('<option/>', { 
+        value: index,
+        text : value 
+    }));
+});
+}
+
 function progressfun(){
     $('#result').empty();
     var Files = document.getElementById('choose_file').files[0]
@@ -19,6 +33,8 @@ function progressfun(){
                 $('#success').css("display", "block");
                 $('#finish').prop("disabled", false);
                 $('#prev').prop("disabled", true);
+                populatedropdown();
+                flag_upload = 1;
             }
         }
 }
@@ -50,6 +66,50 @@ $("#finish").click(function(){
         $('#step3tag').addClass('active');
         $('#step3').addClass('active');
         $('#step3').css("display","block");
+        loadStatus();
     }
 });
 
+function getDropDownList(name, id, optionList) {
+    var select_list = $('<select class="selectpicker btn btn-default"></select>').attr("id", name);
+
+    $.each(optionList, function (i, el) {
+        select_list.append( new Option(el,i) );
+    });
+    $('#maptable tbody').append('<tr id="tr'+ name+'"></tr>');
+    var row_name = "#tr"+name
+    $(row_name).append('<td id="td'+ name+'"></td>');
+    var col_name = "#td"+name
+    $(col_name).append(select_list);
+    flag_map = 1;
+}
+
+$('#add_select').click(function(){
+    param = "#"+active+" option:selected";
+    var selected = $(param).val();
+    alert(selected);
+    var name = 'globallist'+selected;
+    active = name;
+    var copy_globalfields = globalfields
+    var new_append_list = copy_globalfields.splice(selected,1);
+    var select_list = getDropDownList(name,selected,copy_globalfields)
+})
+
+
+function loadStatus() {
+    if (flag_upload == 1){
+        $('#contentstep3').empty().append('<h2>Your file is being mapped</h2>');
+        $('#contentstep3').append('<li><h5>File Uploaded</h5><i class="fa fa-check-circle fa-2x" id="status_icon"></i></li>');
+    }
+    else{
+        $('#contentstep3').append('<h2>Your file is yet to be mapped</h2>');
+        $('#contentstep3').append('<ul>');
+        $('#contentstep3').append('<li><h5>File Uploading</h5><i class="fa fa-spinner fa-2x" id="status_icon"></i></li>');
+    }
+    if (flag_map == 1){
+        $('#contentstep3').append('<li><h5>Cleaning Data</h5><i class="fa fa-check-circle fa-2x" id="status_icon"></i></li>');
+    }
+    else{
+        $('#contentstep3').append('<li><h5>Cleaning Data</h5><i class="fa fa-spinner fa-2x" id="status_icon"></i></li>');
+    }
+}
